29002d472d8535f0303264b33a47e92ee7778f70
ToastMasterson <tylercmasterson@gmail.com>
2020-07-05 09:38:05 -0600

    Fixes Sidebar Nav to include active key

--- imports/ui/components/Sidebar.js
+++ imports/ui/components/Sidebar.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav'
-import Col from 'react-bootstrap/Col'
 
 const Sidebar = ({notes, handleNotes}) => {
 
     return (
-        <Nav variant='pills'>
-            <Col>
-                {notes.map(note => (
-                    <Nav.Item>
-                        <Nav.Link onClick={() => handleNotes(note)} action="true">{note.title}</Nav.Link>
-                    </Nav.Item>
-                ))}
-            </Col>
+        <Nav variant="pills" className="flex-column" defaultActiveKey={'/' + notes[0].title}>
+            {notes.map(note => (
+                <Nav.Item>
+                    <Nav.Link onClick={() => handleNotes(note)} eventKey={'/' + note.title}>{note.title}</Nav.Link>
+                </Nav.Item>
+            ))}
         </Nav>
     )
 }
